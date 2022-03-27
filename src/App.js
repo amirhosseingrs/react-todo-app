861fd3d9cecb5c72309975147161e4807f8bec18
Amir Hossein Garousi <amirhosseingarousi@gmail.com>
2022-03-28 00:19:42 +0430

    First Release

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useEffect } from "react";
+import Todos from "./components/Todos";
+import {v4 as uuidv4} from "uuid";
 
 function App() {
+  const LocalStorage_Key = "todos.list";
+  const todoRef = useRef();
+  const [todos, setTodos] = useState([
+    { id: 1, name: "first todo", completed: false },
+  ]);
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LocalStorage_Key));
+    setTodos(storedTodos);
+  }, []);
+  useEffect(() => {
+    localStorage.setItem(LocalStorage_Key, JSON.stringify(todos));
+  }, [todos]);
+
+  const ToggleTodo = (id) => {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.completed = !todo.completed
+    setTodos(newTodos)
+  };
+
+  const AddtodoHandler = (e) => {
+    if (todoRef.current.value === "") return;
+    const newTodo = {
+      id: uuidv4(),
+      name: todoRef.current.value,
+      completed: false,
+    };
+    setTodos((prevTodos) => {
+      return [...prevTodos, newTodo];
+    });
+    todoRef.current.value = null;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <label>press Enter to Add new todo</label>
+      <br />
+      <input onKeyDown={(e) => {if (e.key === "Enter") AddtodoHandler()}} type="text" ref={todoRef} placeholder="new todo ..."></input>
+      <br />
+      <Todos todos={todos} ToggleTodo={ToggleTodo} />
+      <button onClick={() => setTodos([])}>Clear Todos</button>
+      <br />
+      <label>{todos.length} left todos</label>
+    </>
   );
 }
 
